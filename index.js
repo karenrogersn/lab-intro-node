ef5ce604d9a7ca2f1e307f2d7fd3854d19ae9b61
karenrogersn <56484176+karenrogersn@users.noreply.github.com>
2020-05-17 19:02:27 +0100

    Solved lab

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 class SortedList {
-  constructor() {
-    // ...
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    //adds the value item to the items array
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length++;
   }
 
   load(pos) {
-    // ...
+    //get the value at index position
+    const valueAtIndex = this.items[pos];
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return valueAtIndex;
   }
 
   max() {
-    // ...
+    let maxNumber = Math.max(...this.items);
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return maxNumber;
   }
 
   min() {
-    // ...
+    let minNumber = Math.min(...this.items);
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return minNumber;
   }
 
   sum() {
-    // ...
+    let sum = this.items.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+    return sum;
   }
 
   avg() {
-    // ...
+    let sum = this.items.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+    let average = sum / this.items.length;
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return average;
   }
 }
 
